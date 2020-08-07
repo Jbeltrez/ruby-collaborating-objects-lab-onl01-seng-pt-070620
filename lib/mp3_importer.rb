41d1a918e36c080ee15ad5629db2487719f217de
john beltrez <beltrezjohn@gmail.com>
2020-08-07 10:10:01 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,7 +10,6 @@ class MP3Importer
    
   end 
   def import 
-    self.files.each do |file| file.Song.new_by_filename(file)
-  end 
+    self.files.each {|file| file.Song.new_by_filename(file)}
   end 
 end 
