d51e9f4afc404869b57a564e015e752ba6b4405c
john beltrez <beltrezjohn@gmail.com>
2020-08-07 10:06:29 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,7 +7,7 @@ class MP3Importer
   def files
     @files = Dir.entries(@path)
     @files.delete_if {|file| file == ".." || "."}
-    binding.pry
+   
   end 
   def import 
     Song.new_by_filename(some_filename)
