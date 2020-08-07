6746217214824eb966b11e4a4bd70e2be7c77390
john beltrez <beltrezjohn@gmail.com>
2020-08-07 10:02:27 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class MP3Importer 
   attr_accessor :path 
   def initialize(path)
@@ -5,6 +6,7 @@ class MP3Importer
   end 
   def files
     @files = Dir.entries(@path)
+    binding.pry
   end 
   def import 
     Song.new_by_filename(some_filename)
